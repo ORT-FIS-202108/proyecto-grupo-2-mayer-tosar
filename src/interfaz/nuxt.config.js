880d40dc6764fef051421518b079a1e353d1f852
Joaquin Mayer <joaquin@vexels.com>
2021-11-18 20:36:34 -0300

    Cambio de titulo

--- src/interfaz/nuxt.config.js
+++ src/interfaz/nuxt.config.js
@@ -9,8 +9,8 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - interfaz',
-    title: 'interfaz',
+    titleTemplate: '%s - Mis Gastos',
+    title: 'Inicio',
     htmlAttrs: {
       lang: 'en'
     },
